a14b2cd9d7356ae99dd588ebddfe3d3e49f55a30
Horhe305 <klsolina@gmail.com>
2024-02-22 20:12:00 +0100

    added if __name__ = __main__ and little fix

--- timer.py
+++ timer.py
@@ -1,12 +1,18 @@
 import time
 import sys
 
-t = int(input('How many seconds? '))
-while t:
-    mints, secs = divmod(t, 60)
-    sys.stdout.write('{:02d}:{:02d}'.format(mints, secs))
-    time.sleep(1)
-    sys.stdout.write('\r')
-    t -= 1
 
-print('Timer completed')
+def counter(t):
+    while t:
+        mints, secs = divmod(t, 60)
+        sys.stdout.write('{:02d}:{:02d}'.format(mints, secs))
+        time.sleep(1)
+        sys.stdout.write('\r')
+        t -= 1
+
+    print('Timer completed')
+
+
+if __name__ == '__main__':
+    sec = int(input('How many seconds? '))
+    counter(sec)
