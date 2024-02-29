3d46e2ec1172f594346c645b998dc111150cb8de
Horhe305 <klsolina@gmail.com>
2024-02-29 16:26:13 +0100

    Reformatted code with PEP-8 standards

--- passwords.py
+++ passwords.py
@@ -1,31 +1,33 @@
-import random
-
-'''Generating characters'''
-
-all_chars = ''
-
-# Range of Unicode characters
-start_unicode = 33  # Starting Unicode for characters
-end_unicode = 127  # Ending Unicode for characters
-
-# Generating characters
-for unicode_val in range(start_unicode, end_unicode + 1):
-    char = chr(unicode_val)  # char() converts Unicode number to actual character
-    all_chars += char
+"""Generating random password module."""
+from random import choice
 
 
-def gen_password(chars, how_long):
-    password = ''
-    max_range = len(chars)
+def get_all_characters_in_range(start_unicode: int, end_unicode: int) -> list[str]:
+    """
+    Get a list of all characters in provided unicode ranges.
+    :param start_unicode: Start of the unicode range.
+    :param end_unicode: End of the unicode range.
+    :return: List of characters in the range.
+    """
+    return [chr(character) for character in range(start_unicode, end_unicode + 1)]
 
-    while how_long:
-        password += chars[random.randint(0, max_range - 1)]
-        how_long -= 1
+
+def generate_random_password(length: int = 8, start_unicode: int = 33, end_unicode: int = 127) -> str:
+    """
+    Generate a random password.
+    :param length: Length of the password.
+    :param start_unicode: Start of the unicode chars range for the password.
+    :param end_unicode: End of the unicode chars range for the password.
+    :return: Random password.
+    """
+    available_chars = get_all_characters_in_range(start_unicode, end_unicode)
+    password = ''.join([choice(available_chars) for _ in range(length)])
     return password
 
 
-quantity = int(input('How many passwords do you want? '))
-quality = int(input('How many characters you want in your password(s)? '))
-while quantity:
-    print(gen_password(all_chars, quality))
-    quantity -= 1
+if __name__ == '__main__':
+    quantity = int(input('How many passwords do you want? '))
+    quality = int(input('How many characters you want in your password(s)? '))
+    while quantity:
+        print(generate_random_password(quality))
+        quantity -= 1
