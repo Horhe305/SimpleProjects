8efd8af4e390fcdfa411068bc3429f5343c78729
Horhe305 <klsolina@gmail.com>
2024-07-04 19:03:48 +0200

    changes

--- passowrds_with_classes.py
+++ passowrds_with_classes.py
@@ -1,9 +1,13 @@
-"""Generating random password module."""
-
 from random import choice
 
 
 class PasswordGenerator:
+    """
+    Represents password generation functionality.
+    It allows creating random passwords from a range of Unicode characters specified by the user.
+    By default, the range includes characters from 33 to 127, which covers most printable ASCII characters
+    """
+
     def __init__(self, start_unicode: int = 33, end_unicode: int = 127):
         """
         Initialize the password generator with the optional parameters.
@@ -22,17 +26,25 @@ class PasswordGenerator:
 
     def generate_random_password(self, length: int = 8) -> str:
         """
-        Generate a random password.
+        Generates a random password.
         :param length: Length of the password.
         :return: Random password.
         """
         password = ''.join([choice(self.available_chars) for _ in range(length)])
         return password
 
+    def run(self) -> None:
+        """
+        Prompts the user for input to generate a specified number of passwords with specified length.
+        Then prints the generated passwords one by one.
+        """
+        num_of_passwords = int(input('How many passwords do you want? '))
+        len_of_passwords = int(input('How many characters you want in your password(s)? '))
+        while num_of_passwords:
+            print(password_generator.generate_random_password(len_of_passwords))
+            num_of_passwords -= 1
+
 
 if __name__ == '__main__':
-    quantity = int(input('How many passwords do you want? '))
-    quality = int(input('How many characters you want in your password(s)? '))
-    while quantity:
-        print(PasswordGenerator().generate_random_password(int(quality)))
-        quantity -= 1
+    password_generator = PasswordGenerator()
+    password_generator.run()
